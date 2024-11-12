a12b74963aa02f1ad9facae81a2c53a4b389287b
Erza Bërbatovci <116487812+erza-berbatovci@users.noreply.github.com>
2024-11-12 19:06:47 +0100

    Add Excel export button and URL path for `export_to_excel` view

--- fraud_detection/fraud_detection/urls.py
+++ fraud_detection/fraud_detection/urls.py
@@ -4,11 +4,14 @@ from detection import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from . import views
+
+from detection import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('fraud_detection/', views.fraud_detection_view, name='fraud_detection'),
     path('', views.fraud_detection_view, name='home'),
     path('export/csv/', views.export_to_csv, name='export_to_csv'),
+    path('export/excel/', views.export_to_excel, name='export_to_excel'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
